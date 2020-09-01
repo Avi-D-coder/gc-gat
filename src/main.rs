fc45060d2cbcf57694b4d999ebbf97e4cbf50430
Avi Dessauer <avi.the.coder@gmail.com>
2020-08-31 23:54:18 -0400

    Hmm

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,96 @@
 #![allow(incomplete_features)]
 #![feature(generic_associated_types)]
 #![feature(specialization)]
-#![feature(marker_trait_attr)]
-#![feature(associated_type_defaults)]
 #![feature(negative_impls)]
 #![feature(optin_builtin_traits)]
+
+use auto_traits::Immutable;
 
 fn main() {
     println!("Hello, world!");
 }
 
-/// Should be implemented with each `Trace` impl.
-pub auto trait NotDerived {}
-impl<'l, T> !NotDerived for Gc<'l, T> {}
-
-unsafe trait Life {
-    type L<'l>: 'l + Life;
+pub unsafe trait Life: Immutable {
+    type L<'l>: 'l + Life + Immutable;
 }
 
-unsafe impl<'r, T: 'static + NotDerived> Life for T {
-    type L<'l> = T;
+unsafe impl<T: 'static + Immutable> Life for T {
+    default type L<'l> = T;
 }
 
-struct Gc<'r, T>(&'r T);
+pub struct Gc<'r, T: Life>(&'r T);
 unsafe impl<'r, T: Life> Life for Gc<'r, T> {
     type L<'l> = Gc<'l, T::L<'l>>;
+}
+
+impl<'r, T: Life> Copy for Gc<'r, T> {}
+impl<'r, T: Life> Clone for Gc<'r, T> {
+    fn clone(&self) -> Self {
+        *self
+    }
 }
 
 unsafe impl<'r, T: Life> Life for Option<T> {
     type L<'l> = Option<T::L<'l>>;
 }
 
-impl<T> !NotDerived for Option<T> {}
+mod auto_traits {
+    use super::Gc;
+    use crate::Life;
+    use std::cell::UnsafeCell;
 
+    pub unsafe auto trait NoGc {}
+    impl<'r, T> !NoGc for Gc<'r, T> {}
+    // unsafe impl<'r, T: NoGc> NoGc for Box<T> {}
+
+    pub trait HasGc {
+        const HAS_GC: bool;
+    }
+
+    impl<T> HasGc for T {
+        default const HAS_GC: bool = true;
+    }
+
+    impl<T: NoGc> HasGc for T {
+        const HAS_GC: bool = false;
+    }
+
+    /// Shallow immutability
+    pub unsafe auto trait Immutable {}
+    impl<T> !Immutable for &mut T {}
+    impl<'r, T> !Immutable for &'r T {}
+    impl<T> !Immutable for UnsafeCell<T> {}
+    unsafe impl<T> Immutable for Box<T> {}
+    unsafe impl<'r, T: Life> Immutable for Gc<'r, T> {}
+
+    /// Should be implemented with each `Trace` impl.
+    pub auto trait NotDerived {}
+    impl<'l, T> !NotDerived for Gc<'l, T> {}
+}
+
+#[derive(Copy, Clone)]
+pub struct List<'r, T: Life>(Option<Gc<'r, Elem<'r, T>>>);
+
+#[derive(Clone)]
+pub struct Elem<'r, T: Life> {
+    next: List<'r, T>,
+    value: T,
+}
+
+impl<'r, T: Life + Copy> Copy for Elem<'r, T> {}
 unsafe impl<'r, T: Life> Life for List<'r, T> {
     type L<'l> = List<'l, T::L<'l>>;
 }
-
 unsafe impl<'r, T: Life> Life for Elem<'r, T> {
     type L<'l> = Elem<'l, T::L<'l>>;
 }
 
-impl<'r, T> !NotDerived for Elem<'r, T> {}
-impl<'r, T> !NotDerived for List<'r, T> {}
-
-struct List<'r, T: Life>(<Option<Gc<'r, Elem<'r, T::L<'r>>>> as Life>::L<'r>);
-struct Elem<'r, T: Life> {
-    next: List<'r, T::L<'r>>,
-    value: T::L<'r>,
+#[test]
+fn test() {
+    #![allow(unreachable_code)]
+    let _: List<List<usize>> = todo!();
+    let _: List<List<Gc<String>>> = todo!();
+    let _: List<Gc<String>> = todo!();
+    let _: List<Gc<String>> = todo!();
+    // let _: List<List<Gc<&usize>>> = todo!(); //~ Err the trait bound `&usize: auto_traits::Immutable` is not satisfied
 }
